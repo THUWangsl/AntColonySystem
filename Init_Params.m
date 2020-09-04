1842667eab32e578fb2c39b67272d13e4dbc777e
wangsl <wang-sl18@mails.tsinghua.edu.cn>
2020-09-04 21:00:45 +0800

    add Init_Params.m

---
+++ Init_Params.m
@@ -0,0 +1,10 @@
+% Initital Parameters
+Params.num_UAVs = 6;
+
+% cell{1} is the number of areas, cell{2:end} is the corresponding area
+% vertex ordered colockwise or anticolockwise, which are N by 2 matrixs
+Params.mission_area = cell(1,2);
+Params.prohibited_area = cell(1,2);
+Params.interference_area = cell(1,2);
+Params.obstacle_area = cell(1,2);
+
